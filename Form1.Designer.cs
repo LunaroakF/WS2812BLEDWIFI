56d4be03b3421be15877859e09e4630182d73dd6
Luarshey <smallkay063@outlook.com>
2023-06-14 10:10:11 +0800

    6

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 80);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "地址";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.0.121";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "端口";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "610";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Location = new System.Drawing.Point(5, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(186, 169);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "自定信息";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 21);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox3.Size = new System.Drawing.Size(171, 105);
+            this.textBox3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "上传";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.trackBar1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(199, 11);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(98, 262);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "屏幕灯效";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "启用";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 45);
+            this.trackBar1.Maximum = 3;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(86, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "取色缩放比例";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "90%";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(303, 285);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "110珠 WS2812B远程";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +15,10 @@ namespace WS2812BLEDWIFI
 {
     public partial class Form1 : Form
     {
+        public IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.0.121"), 610);
+        //定义网络类型，数据连接类型和网络协议UDP
+        public Socket PublicRemote = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        public Bitmap screen = null;
         public Form1()
         {
             InitializeComponent();
@@ -22,12 +29,43 @@ namespace WS2812BLEDWIFI
             var point = new Point();
             point.X = 1440;
             point.Y = 1;
-            MessageBox.Show(GetColorFromScreen(point).ToString());
+            ConnectItem();
+            //MessageBox.Show(GetColorFromScreen(point).R.ToString());
+        }
+        public void ConnectItem()//连接目标
+        {
+            //PublicRemote.Close();
+            //PublicRemote.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6001));//绑定端口号和IP
+
+            //PublicRemote = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+            //IPEndPoint ipp = new IPEndPoint(IPAddress.Parse("192.168.0.121"), 610);
+            //PublicRemote.Connect(ipp);
         }
 
-
-        public Bitmap CaptureFromScreen(Rectangle rect)
+        public List<byte[]> SplitList(byte[] superbyte, int size)
         {
+            List<byte[]> result = new List<byte[]>();
+            int length = superbyte.Length;
+            int count = length / size;
+            int r = length % size;
+            for (int i = 0; i < count; i++)
+            {
+                byte[] newbyte = new byte[size];
+                newbyte = superbyte.Skip(size * i).Take(size).ToArray();// SplitArray(superbyte, size*i, size * i+ size);
+                result.Add(newbyte);
+            }
+            if (r != 0)
+            {
+                byte[] newbyte = new byte[r];
+                newbyte = superbyte.Skip(length - r).Take(r).ToArray();
+                result.Add(newbyte);
+            }
+            return result;
+        }
+
+        public Bitmap CaptureFromScreen()
+        {
+            Rectangle rect = Rectangle.Empty;
             Bitmap bmpScreenCapture = null;
             if (rect == Rectangle.Empty)//capture the whole screen
             {
@@ -35,24 +73,99 @@ namespace WS2812BLEDWIFI
             }
             bmpScreenCapture = new Bitmap(rect.Width, rect.Height);
             Graphics p = Graphics.FromImage(bmpScreenCapture);
-
             p.CopyFromScreen(rect.X,
                      rect.Y,
                      0, 0,
                      rect.Size,
                      CopyPixelOperation.SourceCopy);
-
             p.Dispose();
             return bmpScreenCapture;
         }
         public Color GetColorFromScreen(Point p)
         {
-            Rectangle rect = new Rectangle(p, new Size(2, 2));
-            Bitmap map = CaptureFromScreen(rect);
-            Color c = map.GetPixel(0, 0);
-            map.Dispose();
+            Color c = screen.GetPixel(p.X, p.Y);
+            //screen.Dispose();
             return c;
         }
+        public void Upload(string message, Socket Remote)
+        {
+            try
+            {
+                Remote.SendTo(Tobt(message), Tobt(message).Length, SocketFlags.None, ipep);
+                //List<byte[]> a = SplitList(Tobt(message), 1024);
+                //Remote.Send(Tobt(message));//向目标机发送消息1
+            }
+            catch (Exception)
+            {
+                //ConnectItem();
+                throw;
+            }
+            
+        }
 
+
+
+        static byte[] Tobt(string mgs)
+        {
+            byte[] cmgs = Encoding.UTF8.GetBytes(mgs);
+            return cmgs;
+        }
+        public void AutoRun()
+        { //上下35 左右20   2560*1440
+          //横初始像素 73公差71，竖初始像素 80公差68
+            while (true) 
+            {
+                System.Threading.Thread.Sleep(8);
+                var point = new Point();
+                screen = CaptureFromScreen();
+                String H1 = null;
+                String H2 = null;
+                String V1 = null;
+                String V2 = null;
+                //横向
+                for (int i = 0; i < 2; i++)
+                {
+                    if (i == 0) { point.Y = 1296; }
+                    else{ point.Y = 144; }
+                    for (int j = 0; j < 35; j++)
+                    {
+                        if (i == 0){ point.X = 2560 - 2 - ((j + 1) * 71);}
+                        else { point.X = 0 + 2 + ((j + 1) * 71); }
+                        String R = GetColorFromScreen(point).R.ToString();
+                        String G = GetColorFromScreen(point).G.ToString();
+                        String B = GetColorFromScreen(point).B.ToString();
+                        if (i == 0){H1 = H1 + R + "," + G + "," + B + "-";}
+                        else { H2 = H2 + R + "," + G + "," + B + "-"; }
+                    }
+                }
+                //纵向
+                for (int i = 0; i < 2; i++)
+                {
+                    if (i == 0) { point.X = 256; }
+                    else { point.X = 2304; }
+                    for (int j = 0; j < 20; j++)
+                    {
+                        if (i == 0) { point.Y = 1440 - 2 - ((j + 1) * 68); }
+                        else { point.Y = 0 + 2 + ((j + 1) * 68); }
+                        String R = GetColorFromScreen(point).R.ToString();
+                        String G = GetColorFromScreen(point).G.ToString();
+                        String B = GetColorFromScreen(point).B.ToString();
+                        if (i == 0) { V1 = V1 + R + "," + G + "," + B + "-"; }
+                        else { V2 = V2 + R + "," + G + "," + B + "-"; }
+                    }
+                }
+                String Result = H1 + V1 + H2 + V2 + "1-";
+                //MessageBox.Show(Result);
+                Upload(Result,PublicRemote);
+                screen.Dispose();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button2.Text = "运行中";
+            Thread thread = new Thread(new ThreadStart(AutoRun));
+            thread.Start();
+        }
     }
 }
